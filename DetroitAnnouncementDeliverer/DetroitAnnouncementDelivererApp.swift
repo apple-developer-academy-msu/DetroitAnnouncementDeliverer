cd50367f69f09d3af1a4e0030d88e9599aed5505
Tom Phillips <tomphillipsmusic@gmail.com>
2024-10-28 13:34:17 -0400

    Initial Commit

---
+++ DetroitAnnouncementDeliverer/DetroitAnnouncementDelivererApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetroitAnnouncementDelivererApp.swift
+//  DetroitAnnouncementDeliverer
+//
+//  Created by Tom Phillips on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DetroitAnnouncementDelivererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DetroitAnnouncementDelivererTests/DetroitAnnouncementDelivererTests.swift
@@ -0,0 +1,17 @@
+//
+//  DetroitAnnouncementDelivererTests.swift
+//  DetroitAnnouncementDelivererTests
+//
+//  Created by Tom Phillips on 10/28/24.
+//
+
+import Testing
+@testable import DetroitAnnouncementDeliverer
+
+struct DetroitAnnouncementDelivererTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
